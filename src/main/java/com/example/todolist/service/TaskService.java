7d00a94114f85a85a799e06c0389369412d3154e
Thiago0013 <ianthigo13@gmail.com>
2025-11-23 16:56:52 -0300

    Arrumando um pequeno erro.

--- src/main/java/com/example/todolist/service/TaskService.java
+++ src/main/java/com/example/todolist/service/TaskService.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @Service
 public class TaskService {
 
-    protected final TaskRepository repository;
+    private final TaskRepository repository;
 
     public TaskService(TaskRepository repository) {
         this.repository = repository;
