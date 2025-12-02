23de3ecc9e24048e1232ded7511d8f2de489d174
Thiago0013 <ianthigo13@gmail.com>
2025-12-01 23:47:40 -0300

    criando classes DTO, Controller e Service de autenticação de usuario

---
+++ src/main/java/com/example/todolist/controller/AuthController.java
@@ -0,0 +1,56 @@
+package com.example.todolist.controller;
+
+import com.example.todolist.dto.AuthenticationDTO;
+import com.example.todolist.dto.LoginDTO;
+import com.example.todolist.dto.RegisterDTO;
+import com.example.todolist.model.User;
+import com.example.todolist.repository.UserRepository;
+import com.example.todolist.service.TokenService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        var token = tokenService.generateToken((User) auth.getPrincipal());
+
+        return ResponseEntity.ok(new LoginDTO(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
+        if(this.repository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+
+        User newUser = new User(data.login(), encryptedPassword);
+
+        this.repository.save(newUser);
+
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/example/todolist/dto/AuthenticationDTO.java
@@ -0,0 +1,4 @@
+package com.example.todolist.dto;
+
+public record AuthenticationDTO(String login, String password) {
+}
