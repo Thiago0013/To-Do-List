47a3021990d899584ff91e7766b37f5b20a1a76c
Thiago0013 <ianthigo13@gmail.com>
2025-12-01 23:48:38 -0300

    Criando DTO de Login e Register para a entrada dos dados para registrar novo usuario ou logar no sistema.

---
+++ src/main/java/com/example/todolist/dto/LoginDTO.java
@@ -0,0 +1,4 @@
+package com.example.todolist.dto;
+
+public record LoginDTO(String token) {
+}
---
+++ src/main/java/com/example/todolist/dto/RegisterDTO.java
@@ -0,0 +1,4 @@
+package com.example.todolist.dto;
+
+public record RegisterDTO(String login, String password) {
+}
